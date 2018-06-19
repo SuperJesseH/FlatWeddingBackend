ef8770678a4672a9f06019b8b62420bf1f06f3ee
celestegerard <celeste.gerard@gmail.com>
2018-06-19 14:10:19 -0400

    seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-jesse = User.create([{first_name: 'Jesse'}, {last_name: 'Horwitz'}, {street: 'Bakers Street'}])
-alec = User.create([{first_name: 'Alec'}, {last_name: 'Horwitz'}, {street: 'Bakers Street'}])
-bob = User.create([{first_name: 'Bob'}, {last_name: 'Horwitz'}, {street: 'Bakers Street'}])
+jesse = User.create([{first_name: 'Jesse', last_name: 'Horwitz', street: 'Bakers Street'}])
+alec = User.create([{first_name: 'Alec', last_name: 'Horwitz', street: 'Bakers Street'}])
+bob = User.create([{first_name: 'Bob', last_name: 'Horwitz', street: 'Bakers Street'}])
 
 
-first = Post.create([{name: 'first'}, {body: 'hyyyeeee'}, {user_id:1}])
-second = Post.create([{name: 'second'}, {body: 'hyyyeeeeoooo'}, {user_id:2}])
-third = Post.create([{name: 'third'}, {body: 'hyyyeeeeaaaahhh'}, {user_id:3}])
+first = Post.create([{name: 'first', body: 'hyyyeeee', user_id:1}])
+second = Post.create([{name: 'second', body: 'hyyyeeeeoooo', user_id:2}])
+third = Post.create([{name: 'third', body: 'hyyyeeeeaaaahhh', user_id:3}])
 
 
-event1 = Event.create([{name: 'eventone'}, {user_id:1}])
-event2 = Event.create([{name: 'eventtwo'}, {user_id:2}])
-event3 = Event.create([{name: 'eventthree'}, {user_id:3}])
+event1 = Event.create([{name: 'eventone', user_id:1}])
+event2 = Event.create([{name: 'eventtwo', user_id:2}])
+event3 = Event.create([{name: 'eventthree', user_id:3}])

de9f3d1e6ce8c5309c8a39b8d5140f2c96302f61
celestegerard <celeste.gerard@gmail.com>
2018-06-21 12:09:29 -0400

    posts persist now!

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,6 +5,23 @@ class Api::V1::PostsController < ApplicationController
     render json: @posts
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(
+      :name,
+      :body,
+      :user_id
+    )
+  end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :show, :create]
       resources :events, only: [:index]
-      resources :posts, only: [:index]
+      resources :posts, only: [:index, :create]
     end
   end
 end

08317e62a332b673f3b38149ac731908165b3eff
SuperJesseH <therealjhorwitz@gmail.com>
2018-07-30 15:44:30 -0400

    ignore seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,10 @@
 # third = Post.create([{name: 'third', body: 'hyyyeeeeaaaahhh', user_id:3}])
 #
 #
+jesse = User.create([{first_name: 'Jesse', last_name: 'Horwitz', street: 'Bakers Street', username:"jesseHorwitz",  password:"password"}, {first_name: 'Alec', last_name: 'Horwitz', street: 'Bakers Street', username:"alecHorwitz",  password:"password"}])
+
+
+
 event1 = Event.create([{name: 'Ceremony', user_id:1, start_time:'2019-04-13 [12:00:00]',
 end_time:'2019-04-13 [1:00:00]' }])
 event2 = Event.create([{name: 'Reception', user_id:2, start_time:'2019-04-13 [2:00:00]',
